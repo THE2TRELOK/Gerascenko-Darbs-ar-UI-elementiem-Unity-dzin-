8107177f119313a1f7820587d6688953818f5111
Dmitrijs <ggamersshd@gmail.com>
2022-02-23 14:07:20 +0200

    Ievestas pogas un varoni

---
+++ Assets/Skripti/TekstaIevads.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaIevads : MonoBehaviour {
+	public class VardaParadisana : MonoBehaviour{
+	
+	}
+	public string teksts;
+	public string vecums;
+	public GameObject ievads2;
+	public GameObject ievadesLauks;
+	public GameObject tekstaAttelosana;
+	public void uzglabaTekstu(){
+		teksts = ievadesLauks.GetComponent<Text>().text;
+		vecums = ievads2.GetComponent<Text> ().text;
+		tekstaAttelosana.GetComponent<Text> ().text = "Varonim " +teksts.ToUpper()+", "+vecums+" gadi";
+	}
+}
