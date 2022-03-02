f27278b719aad5551b756a16594b94d0792015b6
Dmitrijs <ggamersshd@gmail.com>
2022-03-02 21:22:41 +0200

    Objektu parvietosana

---
+++ Assets/Skripti/ObjektuParvietosana.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObjektuParvietosana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	private RectTransform transLogs;
+	public Canvas Canva;
+
+	void Start(){
+		transLogs = GetComponent<RectTransform> ();
+	}
+
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Kreisais klikškis uz objekta!");
+	}
+
+	public void OnDrag(PointerEventData notikums){
+		Debug.Log ("Objekts tiek pārvietots!");
+		transLogs.anchoredPosition += notikums.delta / Canva.scaleFactor;	
+	}
+
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log ("Pabeigta objekta pārvietošana!");
+	}
+
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("Uzsakta objekta pārvietošana!");
+	}
+}
