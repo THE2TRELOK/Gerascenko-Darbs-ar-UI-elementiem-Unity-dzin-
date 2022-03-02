5b28f097288770e9e03ba38c7f468d1c33349891
Dmitrijs <ggamersshd@gmail.com>
2022-03-02 23:56:28 +0200

    Pabeigts projekts

---
+++ Assets/Skripti/DropDown.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropDown : MonoBehaviour {
+
+	public GameObject mainigaisAttels;
+	public Sprite[] atteluMasivs;
+	public GameObject garums;
+	public GameObject platums;
+
+
+	public void izkritosais(int skaitlis){
+		if (skaitlis == 0) {
+			mainigaisAttels.GetComponent<Image> ().sprite = atteluMasivs [0];
+		}
+		if (skaitlis == 1) {
+			mainigaisAttels.GetComponent<Image> ().sprite = atteluMasivs [1];
+		}
+
+	}
+	public void Platums(){
+		float sakVertiba = platums.GetComponent<Slider>().value;
+		float sakGarums = garums.GetComponent<Slider>().value;
+		mainigaisAttels.GetComponent <RectTransform>().sizeDelta = new Vector2 (110F * sakVertiba, 220F*sakGarums);
+	}
+	public void Garums(){
+		float sakVertiba = platums.GetComponent<Slider>().value;
+		float sakGarums = garums.GetComponent<Slider>().value;
+		mainigaisAttels.GetComponent <RectTransform>().sizeDelta = new Vector2 (110F * sakVertiba, 220F*sakGarums);
+	}
+}
--- README.md
+++ README.md
@@ -3,12 +3,12 @@ Unity projekts ar UI elementu lietojumu dator spele rust
 
 **Uzdevumu saraksts:**
 - [x] Pielietojot varda un vecuma ievades laukus
-- [x] Izveidotas vair�kas ap��rbu un apr�kojuma kategorijas
+- [x] Izveidotas vairakas ap��rbu un apr�kojuma kategorijas
 - [x] Toggle
-- [ ] drag and drop
-- [ ] ska�as efekti
-- [ ] scroll view
-- [ ] divi slider element
+- [x] drag and drop
+- [x] skanas efekti
+- [x] scroll view
+- [x] divi slider element
 - [ ] Slider elements
-- [ ] Audio source elements
-- [ ] Drop down elements
+- [x] Audio source elements
+- [x] Drop down elements
